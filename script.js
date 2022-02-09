8328ab8d71d7c87dc67d641a77bbe453516d303f
1234alex1234 <verygoog@yandex.ru>
2022-02-09 20:55:08 +0300

    дз бот

--- script.js
+++ script.js
@@ -1,97 +1,36 @@
-// //Переменные
-let rollback = 5;
-
-let title = prompt("Как называется наш проект", "Калькулятор верстки");
-let screens = prompt("Какие типы экранов нужно разработать", "Простые, Сложные, Интерактивные");
-let screenPrice;
-let adaptive;
-
-let service1;
-let servicePrice1;
-let service2;
-let servicePrice2;
-
-let fullPrice;
-let allServicePrices;
-let servicePercentPrice;
-
-
 const isNumber = function (num) {
   return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
-const getAllServicePrices = function () {
-  for (let i = 0; i < 2; i++) {
-    if (i === 0) {
-      service1 = prompt("Какой дополнительный вид услуг нужен", "Cms");
-      servicePrice1 = prompt("Сколько это будет стоить", 5000);
-      while (!isNumber(servicePrice1)) {
-        alert("Введите пожалуйста число!");
-        servicePrice1 = prompt("Сколько это будет стоить");
-      }
-    } else if (i === 1) {
-      service2 = prompt("Какой дополнительный вид услуг нужен", "Метрика");
-      servicePrice2 = prompt("Сколько это будет стоить", 4000);
-      while (!isNumber(servicePrice2)) {
-        alert("Введите пожалуйста число!");
-        servicePrice2 = prompt("Сколько это будет стоить");
-      }
+function one() {
+
+  const someNumberFunc = function getRandomInRange() {
+    return Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  };
+
+  let someNumber = someNumberFunc();
+  console.log(someNumber);
+
+
+  function two() {
+    let yourNumber = prompt("Введите число от 1 до 100");
+
+    if (yourNumber == null) {
+      alert("Игра окончена!");
+    } else if (!isNumber(yourNumber)) {
+      alert("это не число!Введите число!");
+      two();
+    } else if (yourNumber > someNumber) {
+      alert("Число больше!");
+      two();
+    } else if (yourNumber < someNumber) {
+      alert("Число меньше!");
+      two();
+    } else if (yourNumber == someNumber) {
+      alert("Вы выйграли!");
     }
   }
-
-  return Number(servicePrice1) + Number(servicePrice2);
-};
-
-
-function getFullPrice() {
-  fullPrice = Number(screenPrice) + allServicePrices;
-  return fullPrice;
+  two();
 }
 
-const getTitle = function () {
-  title = title.trim();
-  title = title[0].toUpperCase() + title.substring(1).toLowerCase();
-  return title;
-};
-
-const getServicePercentPrices = function () {
-  return fullPrice - fullPrice * (rollback / 100);
-};
-
-function showTypeOf(variable) {
-  console.log(typeof variable);
-}
-
-function getRollbackMassage() {
-  if (fullPrice >= 30000) {
-    return "Даем скидку 10%";
-  } else if (fullPrice < 30000 && fullPrice >= 15000) {
-    return "Даем скидку 5%";
-  } else if (fullPrice >= 0 && fullPrice < 15000) {
-    return "Скидка не предусмотрена";
-  } else {
-    return "Что-то пошло не так!";
-  }
-}
-
-let i = 0;
-do {
-  screenPrice = prompt("Сколько будет стоить данная работа", 20000);
-  if (isNumber(screenPrice)) {
-    i++;
-  } else {
-    alert("Введите пожалуйста число!");
-  }
-} while (i < 1);
-
-
-adaptive = confirm("Нужен ли адаптив на сайте");
-allServicePrices = getAllServicePrices();
-fullPrice = getFullPrice();
-servicePercentPrice = getServicePercentPrices();
-
-
-
-console.log("Стоимость доп услуг", allServicePrices);
-console.log(getRollbackMassage());
-console.log("Стоимость с учетом скидки", servicePercentPrice);
+one();
